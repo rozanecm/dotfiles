46cc1d9baa4be39d6476266f5210c4d1fef89ed8
Matias Rozanec <rozanecm@gmail.com>
2019-06-28 01:11:19 -0300

    add sox

--- linux_essentials.sh
+++ linux_essentials.sh
@@ -1,5 +1,9 @@
 # This script contains .... sarasa
 
+# https://linux.die.net/man/1/sox
+# needed for https://gist.github.com/rozanecm/38f2901c592bdffc40726cb0473318cf
+sudo apt-get install sox
+
 # optionals
 sudo apt-get install sl
 sudo apt-get install tree
